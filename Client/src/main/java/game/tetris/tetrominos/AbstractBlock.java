5956e96b0ec134986cf66a5c91193dc98a6494f3
Mcubix <mr.matemeriau@gmail.com>
2023-10-11 13:36:54 +0200

    Add parameters and methods for the abstract class of the blocks (tetrominos).

--- Client/src/main/java/game/tetris/tetrominos/AbstractBlock.java
+++ Client/src/main/java/game/tetris/tetrominos/AbstractBlock.java
@@ -2,15 +2,21 @@ package game.tetris.tetrominos;
 
 import game.tetris.TetrisGrid;
 import game.tetris.datasrtucture.Direction;
+import javafx.scene.paint.Color;
 
 import java.util.function.Consumer;
 
 public abstract class AbstractBlock {
 
 	private final TetrisGrid grid;
+	private Direction direction;
+	private Color color;
+	private boolean isBlocked;
 
 	private AbstractBlock(TetrisGrid grid) {
 		this.grid = grid;
+		this.direction = Direction.UP;
+		this.isBlocked = false;
 	}
 
 	//TODO: need to think about when to lock things, for instance there is no need to lock the grid if no write happen
@@ -20,8 +26,12 @@ public abstract class AbstractBlock {
 
 	abstract void move(int x, int y);
 	abstract void rotate(Direction dir);
+	abstract void changeColor(Color c);
+	abstract void block();
 
 	abstract boolean canMove(int x, int y);
 	abstract boolean canRotate(Direction dir);
+	abstract boolean canChangeColor();
+	abstract boolean isBlocked();
 
 }
