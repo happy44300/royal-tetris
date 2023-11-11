2ddd13dc882de3e93d9979d75d8abca6ff1c0bf8
gabriel <gabriel.pouplin@etu.univ-nantes.fr>
2023-11-11 16:15:19 +0100

    Création d'un classe abstraite ClientBlock.java pour s'occupe de tout les traitements généralistes des blocks du coté client

---
+++ Client/src/main/java/game/tetris/block/ClientBlock.java
@@ -0,0 +1,77 @@
+package game.tetris.block;
+
+import game.tetris.datastructure.AbstractBlock;
+import game.tetris.datastructure.Point;
+import game.tetris.datastructure.TetrisColor;
+import game.tetris.datastructure.TetrisGrid;
+
+import java.rmi.RemoteException;
+
+public abstract class ClientBlock extends AbstractBlock {
+    Point[] points = new Point[4];
+    TetrisColor COLOR;
+    public ClientBlock(int x, int y, TetrisGrid grid){
+        super(x, y, grid);
+    }
+
+    public void paint() throws RemoteException {
+        this.tetrisGrid.updateGridSynchronously(
+                safeGrid-> {
+                    try {
+                        if (!canTranslateUnsafe(new Point(0,0), safeGrid)) {
+                            throw new RuntimeException("Cannot draw cell on occupied cell");
+                        }
+                        for (Point point : points) {
+                            safeGrid.getCell(point).setColor(COLOR);
+                        }
+
+                    } catch (RemoteException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+        );
+    }
+
+    @Override
+    public void translate(Point toPoint) throws RemoteException {
+        this.tetrisGrid.updateGridSynchronously(
+                safeGrid-> {
+                    try {
+                        /*if (!canTranslateUnsafe(toPoint, safeGrid)) {
+                            return;
+                        }*/
+                        for (Point point : points) {
+                            safeGrid.getCell(point).setColor(TetrisColor.NOTHING);
+                        }
+                        for (Point point : points) {
+                            var gridPoint = safeGrid.getCell(point.add(toPoint));
+                            gridPoint.setColor(COLOR);
+                        }
+
+                    } catch (RemoteException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+        );
+    }
+    private boolean canTranslateUnsafe(Point toPoint, TetrisGrid grid) throws RemoteException {
+        for (Point point : points) {
+            var neededPoint = grid.getCell(point.add(toPoint));
+            if(neededPoint.getColor() != TetrisColor.NOTHING){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean canTranslate(Point point) throws RemoteException {
+        return tetrisGrid.updateGridSynchronously(grid -> {
+            try {
+                return canTranslateUnsafe(point,grid);
+            } catch (RemoteException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+}
--- Client/src/main/java/game/tetris/block/OBlock.java
+++ Client/src/main/java/game/tetris/block/OBlock.java
@@ -6,12 +6,7 @@ import game.tetris.tetrominos.TetrominosTexture;
 import java.rmi.RemoteException;
 import java.security.InvalidParameterException;
 
-public class OBlock extends AbstractBlock {
-
-
-    Point[] points = new Point[4];
-    private static final TetrisColor COLOR = TetrisColor.YELLOW;
-
+public class OBlock extends ClientBlock {
     public OBlock(int x, int y, TetrisGrid grid) {
         super(x, y, grid);
 
@@ -19,55 +14,15 @@ public class OBlock extends AbstractBlock {
         points[1] = new Point(x+1,y);
         points[2] = new Point(x,y+1);
         points[3] = new Point(x+1,y+1);
+        COLOR = TetrisColor.YELLOW;
         try {
             paint();
         } catch (RemoteException e) {
             throw new RuntimeException(e);
         }
-
-
     }
 
-    private void paint() throws RemoteException {
-        this.tetrisGrid.updateGridSynchronously(
-                safeGrid-> {
-                    try {
-                        if (!canTranslateUnsafe(new Point(0,0), safeGrid)) {
-                            throw new RuntimeException("Cannot draw cell on occupied cell");
-                        }
-                        for (Point point : points) {
-                            safeGrid.getCell(point).setColor(COLOR);
-                        }
 
-                    } catch (RemoteException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-        );
-    }
-
-    @Override
-    public void translate(Point toPoint) throws RemoteException {
-        this.tetrisGrid.updateGridSynchronously(
-                safeGrid-> {
-                    try {
-                        /*if (!canTranslateUnsafe(toPoint, safeGrid)) {
-                            return;
-                        }*/
-                        for (Point point : points) {
-                            safeGrid.getCell(point).setColor(TetrisColor.NOTHING);
-                        }
-                        for (Point point : points) {
-                            var gridPoint = safeGrid.getCell(point.add(toPoint));
-                            gridPoint.setColor(COLOR);
-                        }
-
-                    } catch (RemoteException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-        );
-    }
 
 
     @Override
@@ -75,26 +30,7 @@ public class OBlock extends AbstractBlock {
         //null op
     }
 
-    private boolean canTranslateUnsafe(Point toPoint, TetrisGrid grid) throws RemoteException {
-        for (Point point : points) {
-            var neededPoint = grid.getCell(point.add(toPoint));
-            if(neededPoint.getColor() != TetrisColor.NOTHING){
-                return false;
-            }
-        }
-        return true;
-    }
 
-    @Override
-    public boolean canTranslate(Point point) throws RemoteException {
-        return tetrisGrid.updateGridSynchronously(grid -> {
-            try {
-                return canTranslateUnsafe(point,grid);
-            } catch (RemoteException e) {
-                throw new RuntimeException(e);
-            }
-        });
-    }
 
     @Override
     public boolean canRotate(Rotation dir) {
