ad6e55782d158c1f5cbd1987e614db45c7d63a5c
theray1 <theray1@outlook.fr>
2024-01-03 23:31:00 +0100

    ConnectionManager interface update

--- Common/src/main/java/game/tetris/ConnectionManager.java
+++ Common/src/main/java/game/tetris/ConnectionManager.java
@@ -1,5 +1,12 @@
 package game.tetris;
 
-public interface ConnectionManager {
-    void connect();
+import game.tetris.block.Block;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface ConnectionManager extends Remote {
+    void connect() throws RemoteException;
+    void updateGrid(Grid updatedGrid) throws RemoteException;
+    void updateBlock(String playerID, Block updatedBlock);
 }
