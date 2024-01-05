16e29ce24b3b4b9e735d40bbdc07a36981a6a0b5
gabriel <gabriel.pouplin@etu.univ-nantes.fr>
2024-01-05 10:53:12 +0100

    Ajout de la methode connect

--- Client/src/main/java/game/tetris/TetrisApplication.java
+++ Client/src/main/java/game/tetris/TetrisApplication.java
@@ -3,13 +3,19 @@ package game.tetris;
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
 import javafx.scene.input.KeyCode;
-
+import game.tetris.TetrisGame;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.List;
 import java.util.Map;
 
 import static com.almasb.fxgl.dsl.FXGLForKtKt.getInput;
 
 public class TetrisApplication extends GameApplication {
-
+    TetrisGame game;
+    ConnectionManager connectionManager;
+    private String ip = "127.0.0.1";
     @Override
     protected void initSettings(GameSettings settings) {
         settings.setWidth(1600);
@@ -44,4 +50,32 @@ public class TetrisApplication extends GameApplication {
         getInput().addAction(new RightKeyAction("Move Right", this), KeyCode.RIGHT);
         getInput().addAction(new UpKeyAction("Move UP", this), KeyCode.UP);
     }
+
+    private void connect() {
+        try {
+            Registry remoteRegistry = LocateRegistry.getRegistry(ip, 10000);
+            TetrisGame game = (TetrisGame) remoteRegistry.lookup("TetrisGame");
+
+            Registry localRegistry = LocateRegistry.createRegistry(10001);
+
+            connectionManager = (ConnectionManager) UnicastRemoteObject.
+                    exportObject(new ConnectionManagerClient(), 10001);
+
+            localRegistry.rebind("ConnectionManager", connectionManager);
+
+            /*//logs is a list of two elements : an IP and an ID
+            List<String> logs = game.connect();
+
+            System.setProperty("java.rmi.server.hostname", logs.get(0));
+
+            updateHandler.setID(logs.get(1));
+
+            this.updateHandler = updateHandler;*/
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
